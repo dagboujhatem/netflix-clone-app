72f269ad68e9b92298003446fac97462364bb3b0
Hatem DAGHBOUJ <hatemdagbouj@gmail.com>
2025-02-01 10:19:07 +0100

    fix tests

--- netflix-service-discovery/src/test/java/com/albinus/netflix/services/NetflixServiceDiscoveryApplicationTests.java
+++ netflix-service-discovery/src/test/java/com/albinus/netflix/services/NetflixServiceDiscoveryApplicationTests.java
@@ -1,5 +1,6 @@
 package com.albinus.netflix.services;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class NetflixServiceDiscoveryApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(true);
 	}
 
 }
