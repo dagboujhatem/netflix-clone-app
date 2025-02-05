c06ce989b46f9581d826bbf55e09a85916b2be28
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2025-02-05 16:13:23 +0100

    add ihm service

---
+++ netflix-ihm-service/src/test/java/com/albinus/netflix/services/NetflixIHMServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.albinus.netflix.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixIHMServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
